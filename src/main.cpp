deb9a108522ae2ce9592442c089ba9097de348d3
GionaBolzer <gionabolzer2@gmail.com>
2023-11-12 18:56:17 +0100

    messo il file di configurazione, messo delle variabilie per il comportamento della ventola

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,4 @@
-#include <Arduino.h>
-#include <SoftwareSerial.h>
-#include "PMS.h"
-
-///////////////////////////////////// SENSOR ////////////////////////////////////////////
-#ifdef PMSENSOR
-SoftwareSerial pmsSerial(2, 3);
-PMS pms(pmsSerial);
-PMS::DATA data;
-#define GET_PM_READ_EVERY 120000 // wake sensore every 60s
-#define WAIT_PM 30000            // wait 30s for sensor to stabilize
-unsigned long sensorTimer = 0;
-bool SENSOR_STATE = false; // if false sensor sleep
-uint32_t PM_READ = 0;
-#endif
-
-void wakeUpSensor(); // wake uo every 120s
-
-void readSensor();
-
-///////////////////////////////////// FAN ////////////////////////////////////////////
-
-#ifdef FAN
-#define CHANGE_FAN 60000 // wait 30s for sensor to stabilize
-unsigned long fanTimer = 0;
-const byte OC1A_PIN = 9;
-const byte OC1B_PIN = 10;
-
-const word PWM_FREQ_HZ = 25000; // Adjust this value to adjust the frequency
-const word TCNT1_TOP = 16000000 / (2 * PWM_FREQ_HZ);
-
-uint16_t fanPower = 20; // default power is 20 %
-
-void setPwmDuty(byte duty)
-{
-    OCR1A = (word)(duty * TCNT1_TOP) / 100;
-}
-
-void power(uint32_t read)
-{
-    if (millis() > fanTimer)
-    {
-        float power = 0.88f * float(read) + 11.1f;
-        if (power > 100.f)
-        {
-            power = 100.f;
-        }
-        if (power < 20.f)
-        {
-
-            power = 20.f;
-        }
-        fanPower = int(power);
-#ifdef LOG
-        Serial.print("power: ");
-        Serial.println(fanPower);
-#endif
-        fanTimer = millis() + CHANGE_FAN;
-    }
-}
-#endif
+#include "main.h"
 
 void setup()
 {
@@ -67,7 +7,6 @@ void setup()
 #endif
 
 #ifdef PMSENSOR
-
     pmsSerial.begin(9600);
     pms.passiveMode();
 #endif
@@ -144,5 +83,33 @@ void readSensor()
         pms.sleep();
         SENSOR_STATE = false;
         sensorTimer = millis() + GET_PM_READ_EVERY; // update timer
+    }
+}
+
+void setPwmDuty(byte duty)
+{
+    OCR1A = (word)(duty * TCNT1_TOP) / 100;
+}
+
+void power(uint32_t read)
+{
+    if (millis() > fanTimer)
+    {
+        float power = (float(maxPower - minPower)/float(maxPowerPM - minPowerPM)) * (float(read - minPowerPM)) + float(minPower); // retta passatper per (minPowerPM, minPower) e (maxPowerPM, maxPower)
+
+        if (power > float(maxPower))
+        {
+            power = maxPower;
+        }
+        if (power < float(minPower))
+        {
+            power = minPower;
+        }
+        fanPower = int(power);
+#ifdef LOG
+        Serial.print("power: ");
+        Serial.println(fanPower);
+#endif
+        fanTimer = millis() + CHANGE_FAN;
     }
 }
